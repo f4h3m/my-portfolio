f6c71c38628205b0a252fb45d2fe09ca1c50201d
f4h3m <fahemdj99@gmail.com>
2024-10-04 04:40:20 +0600

    hide some section for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,7 @@ export default function Page() {
             </div>
           </div>
         </section>
-        <section id="projects">
+        {/* <section id="projects">
           <div className="space-y-12 w-full py-12">
             <BlurFade delay={BLUR_FADE_DELAY * 11}>
               <div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -204,7 +204,7 @@ export default function Page() {
               </ul>
             </BlurFade>
           </div>
-        </section>
+        </section> */}
         <section id="contact">
           <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
             <BlurFade delay={BLUR_FADE_DELAY * 16}>
